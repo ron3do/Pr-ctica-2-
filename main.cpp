31b2d83d194d5d723f32de00a65b8f07cb0a15c7
RONALDEDUARDOED <>
2021-05-01 23:23:32 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  int num=0;
+  cout<<"Digite un número entre 0 y 999"<<endl;
+  cin>>num;
+
+  if (num<0 && num>999){
+    switch(num)
+
+  }
+  else {
+    cout<<"Por Favor digite un numero entre 0 y 999"<< endl;
+    break;
+    }
+}
+}
