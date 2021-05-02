eaddeddc6435360c0e703c4319f3bba0b2ac0a68
RONALD EDUARDO EDUARDO <>
2021-05-02 14:00:33 +0000

    se realizo un programa funcional, pero falta de comentar. Y se tratara de hacer con la funion switch

--- main.cpp
+++ main.cpp
@@ -6,92 +6,143 @@ int main() {
   
   cout<<"Digite un número entre 0 y 999"<< endl;
   cin>>num;
-  if (num>1 && num<999) {
+  if (num>=0 && num<1000) {
     
-    if (num>=900 && num<999){
+    if (num>=900 && num<=999){
       cout<<"novecientos ";
       num -=900;
     }
-    if (num>=800 && num<899){
+      else if (num>=800 && num<=899){
       cout<<"ochocientos ";
       num -=800;
     }
-    if (num>=700 && num<799){
+      else if (num>=700 && num<=799){
       cout<<"setecientos ";
       num -=700;
     }
-    if (num>=600 && num<699){
+      else if (num>=600 && num<=699){
       cout<<"seiscientos ";
       num -=600;
     }
     
-    if (num>=500 && num<599){
+      else if (num>=500 && num<=599){
       cout<<"quinicientos ";
       num -=500;
     }
     
-    if (num>=400 && num<499){
+      else if (num>=400 && num<=499){
       cout<<"cuatrocientos ";
       num -=400;
     }
   
-    if (num>=300 && num<399){
+      else if (num>=300 && num<=399){
       cout<<"trecientos ";
       num -=300;
     }
       
-    if (num>=200 && num<299){
+      else if (num>=200 && num<=299){
       cout<<"dosicientos ";
       num -=200;
     }  
-    if (num>=100 && num<199){
+      else if (num>=100 && num<=199){
       cout<<"ciento ";
       num -=100;
     }   
-    else if (num>=90 && num<99){
-      cout<<"noventa ";
+    if (num>=90 && num<=99){
+      cout<<"noventa y ";
       num -=90;
     }
-    else if (num>=80 && num<89){
-      cout<<"ochenta ";
-      num -=80;
+      else if (num>=80 && num<=89){
+        cout<<"ochenta y ";
+        num -=80;
     }
-    else if (num>=70 && num<79){
-      cout<<"seteta ";
-      num -=70;
+      else if (num>=70 && num<=79){
+        cout<<"setenta y ";
+        num -=70;
     }
-    else if (num>=60 && num<69){
-      cout<<"seseta ";
-      num -=60;
-    }
-    
-    else if (num>50 && num<59){
-      cout<<"cincuenta ";
-      num -=50;
+      else if (num>=60 && num<=69){
+        cout<<"sesenta y ";
+        num -=60;
     }
     
-    else if (num>40 && num<49){
-      cout<<"cuarenta ";
-      num -=40;
+      else if (num>=50 && num<=59){
+        cout<<"cincuenta y ";
+        num -=50;
+    }
+    
+      else if (num>=40 && num<=49){
+        cout<<"cuarenta y ";
+        num -=40;
     }
   
-    else if (num>30 && num<39){
-      cout<<"treinta ";
-      num -=30;
+      else if (num>=30 && num<=39){
+        cout<<"treinta y";
+        num -=30;
     }
       
-    else if (num>20 && num<29){
-      cout<<"veinte  ";
-      num -=20;
+      else if (num>=20 && num<=29){
+        cout<<"veinte y  ";
+        num -=20;
     }  
-    if (num>100 && num<199){
-      cout<<"diez ";
-      num -=10;
+      else if (num>=10 && num<=19){
+        cout<<"diez y ";
+        num -=10;
+      }
+    if (num==9){
+        cout<<"Nueve";
+        num -=9;
     }  
+    if (num==8){
+      cout<<"ocho";
+      num -=8;
+    }
+      else if (num==7){
+        cout<<"Siete";
+        num -=7;
+    }
+      else if (num==6){
+        cout<<"Seis";
+        num -=6;
+    }
+      else if (num==5){
+        cout<<"cinco";
+        num -=5;
+    }
+    
+      else if (num==4){
+        cout<<"Cuatro";
+        num -=4;
+    }
+    
+      else if (num==3){
+        cout<<"tres";
+        num -=3;
+    }
+  
+      else if (num==2){
+        cout<<"dos";
+        num -=2;
+    }
+      
+      else if (num==1){
+        cout<<"Uno ";
+        num -=1;
+    }  
+    /*
+      else if (num==0){
+        cout<<"Cero";
+        
+    }
+     */
     cout<<num;
 
       }
+  else{
+    cout<<num<<" No es un número valido"<<endl;
+    cout<<"Favor de digitar un número del rango permitido "<<endl;
+    main ();
 
+  }
     
    
   
